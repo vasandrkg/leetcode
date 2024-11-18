0c98b7f2516cca61d335c52a63c58e5195677232
vasandrkg <vasandrkg@gmail.com>
2024-11-19 00:34:14 +0300

    Create main.go

---
+++ arrays_and_hashing/contains_duplicate_217/main.go
@@ -0,0 +1,86 @@
+// LeetCode 217.
+// Contains Duplicate.
+// https://leetcode.com/problems/contains-duplicate/description/
+
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: true
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: false
+// Example 3:
+
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// -109 <= nums[i] <= 109
+
+package main
+
+import "fmt"
+
+func main() {
+
+	nums0 := []int{1, 2, 5, 3, 4, 5}
+	nums1 := []int{1, 2, 3, 4, 5}
+	nums2 := []int{1, 2, 5, 3, 4, 4, 5}
+	nums3 := []int{1}
+
+	containsDuplicate(nums0)
+	containsDuplicate2(nums0)
+  
+	containsDuplicate(nums1)
+	containsDuplicate2(nums1)
+	
+  containsDuplicate(nums2)
+	containsDuplicate2(nums2)
+	
+  containsDuplicate(nums3)
+	containsDuplicate2(nums3)
+
+}
+
+// first solution
+func containsDuplicate(nums []int) bool {
+	x := make(map[int]int)
+	var b bool
+
+	if len(nums) == 1 {
+		b = false
+		fmt.Println(b, "from containsDuplicate")
+		return b
+	}
+
+	for i := 0; i < len(nums); i++ {
+		x[nums[i]] += 1
+	}
+	for _, val := range x {
+		if val > 1 {
+			b = true
+			fmt.Println(b, "from containsDuplicate")
+			return b
+		}
+	}
+	b = false
+	fmt.Println(b, "from ontainsDuplicate")
+	return b
+}
+
+//second solution
+func containsDuplicate2(nums []int) bool {
+	s := map[int]bool{}
+
+	for i := 0; i < len(nums); i++ {
+		s[nums[i]] = true
+	}
+	b2 := len(s) != len(nums)
+	fmt.Println(b2, "from containsDuplicate2")
+	return b2
+}
