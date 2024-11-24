66db12afc87b384392d13a79cadd2591895b4ae9
Andrey Vasilev <vasandrkg@gmail.com>
2024-11-24 20:56:47 +0300

    contains duplicate

--- arrays_and_hashing/contains_duplicate_217/main.go
+++ arrays_and_hashing/contains_duplicate_217/main.go
@@ -24,17 +24,20 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 
 	// test data
-	nums0 := []int{1, 2, 5, 3, 4, 5}    // true
-	nums1 := []int{1, 2, 3, 4, 5}       // false
-	nums2 := []int{1, 2, 5, 3, 4, 4, 5} // true
-	nums3 := []int{1}                   // false
+	nums0 := []int{7, 9, 4, 3, 5, 2, 5, 3, 4, 1, 8, 8, 7, 6, 6, 6, 777, 9999} // true
+	nums1 := []int{2, 1, 3, 4, 5}                                             // false
+	nums2 := []int{2, 1, 5, 3, 4, 4, 5}                                       // true
+	nums3 := []int{1}                                                         // false
 
-	fmt.Println("\n\n=======================================================")
+	fmt.Println("\n\n==========================================================")
 	fmt.Println("\t First Solution. My Own Solution. My First One")
 	fmt.Println("===========================================================\n\n")
 
@@ -43,7 +46,7 @@ func main() {
 	containsDuplicate1(nums2)
 	containsDuplicate1(nums3)
 
-	fmt.Println("\n\n=====================================================")
+	fmt.Println("\n\n==========================================================")
 	fmt.Println("\t Second Solution. Hash Set Length")
 	fmt.Println("=========================================================\n\n")
 
@@ -69,6 +72,24 @@ func main() {
 	containsDuplicate4(nums1)
 	containsDuplicate4(nums2)
 	containsDuplicate4(nums3)
+
+	fmt.Println("\n\n=======================================================")
+	fmt.Println("\t Fifth Solution. Sorting. My Own Bubble Sort")
+	fmt.Println("=======================================================\n\n")
+
+	containsDuplicate5(nums0)
+	containsDuplicate5(nums1)
+	containsDuplicate5(nums2)
+	containsDuplicate5(nums3)
+
+	fmt.Println("\n\n============================================================================")
+	fmt.Println("\t Sixth Solution. Sorting. Using \"sort\" package from Golang library")
+	fmt.Println("=============================================================================\n\n")
+
+	containsDuplicate6(nums0)
+	containsDuplicate6(nums1)
+	containsDuplicate6(nums2)
+	containsDuplicate6(nums3)
 
 }
 
@@ -128,5 +149,37 @@ func containsDuplicate4(nums []int) bool {
 		seen[num] = true
 	}
 	fmt.Println(false, " - from function containsDuplicate4, Hash Set, Fourth Solution")
+	return false
+}
+
+func containsDuplicate5(nums []int) bool {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i] > nums[j] {
+				nums[i], nums[j] = nums[j], nums[i]
+			}
+		}
+	}
+	fmt.Printf("sorted nums are: %v\n", nums)
+
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] == nums[i+1] {
+			fmt.Println(true, " - from function containsDuplicate5, Sorting, My Own Bubble Sort, Fifth Solution")
+			return true
+		}
+	}
+	fmt.Println(false, " - from function containsDuplicate5, Sorting, My Own Bubble Sort, Fifth Solution")
+	return false
+}
+
+func containsDuplicate6(nums []int) bool {
+	sort.Ints(nums)
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] == nums[i+1] {
+			fmt.Println(true, " - from function containsDuplicate6, Sorting, From \"sort\" package,  Sixth Solution")
+			return true
+		}
+	}
+	fmt.Println(false, " - from function containsDuplicate6, Sorting,  From \"sort\" package, Sixth Solution")
 	return false
 }
