6d199c564e3cd3885783136643f1713b6f52d8be
Andrey Vasilev <vasandrkg@gmail.com>
2024-11-24 19:48:01 +0300

    contains duplicate

--- arrays_and_hashing/contains_duplicate_217/main.go
+++ arrays_and_hashing/contains_duplicate_217/main.go
@@ -32,20 +32,20 @@ func main() {
 	nums0 := []int{1, 2, 5, 3, 4, 5}    // true
 	nums1 := []int{1, 2, 3, 4, 5}       // false
 	nums2 := []int{1, 2, 5, 3, 4, 4, 5} // true
-	nums3 := []int{1}                   //false
+	nums3 := []int{1}                   // false
 
-	fmt.Println("\n\n==========================================")
-	fmt.Println("\t First Solution")
-	fmt.Println("==========================================\n\n")
+	fmt.Println("\n\n=======================================================")
+	fmt.Println("\t First Solution. My Own Solution. My First One")
+	fmt.Println("===========================================================\n\n")
 
 	containsDuplicate1(nums0)
 	containsDuplicate1(nums1)
 	containsDuplicate1(nums2)
 	containsDuplicate1(nums3)
 
-	fmt.Println("\n\n=========================================")
-	fmt.Println("\t Second Solution")
-	fmt.Println("=========================================\n\n")
+	fmt.Println("\n\n=====================================================")
+	fmt.Println("\t Second Solution. Hash Set Length")
+	fmt.Println("=========================================================\n\n")
 
 	containsDuplicate2(nums0)
 	containsDuplicate2(nums1)
@@ -53,56 +53,80 @@ func main() {
 	containsDuplicate2(nums3)
 
 	fmt.Println("\n\n=========================================")
-	fmt.Println("\t Third Solution")
+	fmt.Println("\t Third Solution. Brute Force")
 	fmt.Println("=========================================\n\n")
 
-	//containsDuplicate3(nums0)
-	//containsDuplicate3(nums1)
-	//containsDuplicate3(nums2)
-	//containsDuplicate3(nums3)
+	containsDuplicate3(nums0)
+	containsDuplicate3(nums1)
+	containsDuplicate3(nums2)
+	containsDuplicate3(nums3)
+
+	fmt.Println("\n\n=========================================")
+	fmt.Println("\t Fourth Solution. Hash set")
+	fmt.Println("=========================================\n\n")
+
+	containsDuplicate4(nums0)
+	containsDuplicate4(nums1)
+	containsDuplicate4(nums2)
+	containsDuplicate4(nums3)
 
 }
 
-// first solution
 func containsDuplicate1(nums []int) bool {
 	x := make(map[int]int)
-	var b bool
 
 	if len(nums) == 1 {
-		b = false
-		fmt.Println(b, "from function containsDuplicate")
-		return b
+		fmt.Println(false, " - from function containsDuplicate1, My Own Solution, First Solution")
+		return false
 	}
 
 	for i := 0; i < len(nums); i++ {
 		x[nums[i]] += 1
 	}
+
 	for _, val := range x {
 		if val > 1 {
-			b = true
-			fmt.Println(b, "from function containsDuplicate")
-			return b
+			fmt.Println(true, " - from function containsDuplicate1, My Own Solution, First Solution")
+			return true
 		}
 	}
-	b = false
-	fmt.Println(b, "from function containsDuplicate")
-	return b
+	fmt.Println(false, " - from function containsDuplicate1, My Own Solution, First Solution")
+	return false
 }
 
-// second solution (hash set length)
 func containsDuplicate2(nums []int) bool {
 	seen := map[int]bool{}
 
 	for i := 0; i < len(nums); i++ {
 		seen[nums[i]] = true
 	}
-	b2 := (len(seen) != len(nums))
-	fmt.Println(b2, "from function containsDuplicate2")
-	return b2
+	fmt.Println(len(seen) != len(nums), " - from function containsDuplicate2, Hash Set Length, Second Solution")
+	return len(seen) != len(nums)
 }
 
-// third solution
+func containsDuplicate3(nums []int) bool {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i] == nums[j] {
+				fmt.Println(true, " - from function containsDuplicate3, Brute Force, Third Solution")
+				return true
+			}
+		}
+	}
+	fmt.Println(false, " - from function containsDuplicate3, Brute Force, Third Solution")
+	return false
+}
 
-//func containsDuplicate3(nums []int) bool {
-//
-//}
+func containsDuplicate4(nums []int) bool {
+	seen := make(map[int]bool)
+
+	for _, num := range nums {
+		if seen[num] {
+			fmt.Println(true, " - from function containsDuplicate4, Hash Set, Fourth Solution")
+			return true
+		}
+		seen[num] = true
+	}
+	fmt.Println(false, " - from function containsDuplicate4, Hash Set, Fourth Solution")
+	return false
+}
