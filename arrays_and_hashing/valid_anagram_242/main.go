4bb39c0b611dd780d606f999579f729d9c5aeed7
Andrey Vasilev <vasandrkg@gmail.com>
2024-11-26 01:50:37 +0300

    valid anagram leetcode 242

--- arrays_and_hashing/valid_anagram_242/main.go
+++ arrays_and_hashing/valid_anagram_242/main.go
@@ -28,6 +28,60 @@
 
 package main
 
+import "fmt"
+
 func main() {
 
+	// test cases
+
+	// false
+	s0 := "cat"
+	t0 := "car"
+	isAnagram(s0, t0)
+
+	// true
+	s1 := "listen"
+	t1 := "silent"
+	isAnagram(s1, t1)
+
+	// true
+	s2 := "newyorktimes"
+	t2 := "monkeyswrite"
+	isAnagram(s2, t2)
+
+	//false
+	s3 := "listen to your hurt"
+	t3 := "silent hill"
+	isAnagram(s3, t3)
+
+}
+
+func isAnagram(s, t string) bool {
+	if len(s) != len(t) {
+		fmt.Println(len(s), len(t))
+		fmt.Printf("false - Not an Anagram. The length of strings [%#s] and [%#s] is not the same.\n\n", s, t)
+		return false
+	}
+	ms := make(map[string]int)
+	for _, vs := range s {
+		ms[string(vs)] += 1
+	}
+	fmt.Printf("ms is: %#v\n", ms)
+	mt := make(map[string]int)
+	for _, vt := range t {
+		mt[string(vt)] += 1
+	}
+	fmt.Printf("mt is: %#v\n", mt)
+
+	//for keyt, _ := range mt {
+	//	for keys, _ := range ms {
+	//		if keyt != keys {
+	//			fmt.Printf("\nfalse - Not an Anagram. There is no [%s] and [%s] is not the same", keyt, keys)
+	//			return false
+	//		}
+	//
+	//	}
+	//}
+
+	return true
 }
