c8305d617909f19b0e718da17ce8a1113eeb6d02
Andrey Vasilev <vasandrkg@gmail.com>
2024-11-26 16:54:58 +0300

    valid anagram leetcode 242

--- arrays_and_hashing/valid_anagram_242/main.go
+++ arrays_and_hashing/valid_anagram_242/main.go
@@ -1,6 +1,6 @@
 // LeetCode 242.
 // Valid Anagram.
-
+// https://leetcode.com/problems/valid-anagram/description/
 // Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 //
 // Example 1:
@@ -38,50 +38,96 @@ func main() {
 	s0 := "cat"
 	t0 := "car"
 	isAnagram(s0, t0)
-
+	isAnagram1(s0, t0)
 	// true
 	s1 := "listen"
 	t1 := "silent"
 	isAnagram(s1, t1)
+	isAnagram1(s1, t1)
 
 	// true
 	s2 := "newyorktimes"
 	t2 := "monkeyswrite"
 	isAnagram(s2, t2)
-
-	//false
+	isAnagram1(s2, t2)
+	// false
 	s3 := "listen to your hurt"
 	t3 := "silent hill"
 	isAnagram(s3, t3)
+	isAnagram1(s3, t3)
+
+	//// more compact view of test cases
+	//ss := []string{"cat", "listen", "newyorktimes", "listen toy your heart"}
+	//tt := []string{"car", "silent", "monkeyswrite",  "silent hill"}
+	//
 
 }
 
+// my own solution
 func isAnagram(s, t string) bool {
 	if len(s) != len(t) {
-		fmt.Println(len(s), len(t))
-		fmt.Printf("false - Not an Anagram. The length of strings [%#s] and [%#s] is not the same.\n\n", s, t)
+		fmt.Printf("false: Not an Anagram. The length of strings [%#s] and [%#s] is not the same.\n\n", s, t)
 		return false
 	}
+
 	ms := make(map[string]int)
-	for _, vs := range s {
-		ms[string(vs)] += 1
-	}
-	fmt.Printf("ms is: %#v\n", ms)
 	mt := make(map[string]int)
-	for _, vt := range t {
-		mt[string(vt)] += 1
+
+	for _, char := range s {
+		ms[string(char)] += 1
 	}
-	fmt.Printf("mt is: %#v\n", mt)
 
-	//for keyt, _ := range mt {
-	//	for keys, _ := range ms {
-	//		if keyt != keys {
-	//			fmt.Printf("\nfalse - Not an Anagram. There is no [%s] and [%s] is not the same", keyt, keys)
-	//			return false
-	//		}
-	//
-	//	}
-	//}
+	for _, char := range t {
+		mt[string(char)] += 1
+	}
 
+	for key, _ := range ms {
+		if ms[key] != mt[key] {
+			fmt.Println("false: Not an Anagram.\n")
+			return false
+		}
+	}
+	fmt.Printf("true: \nms is: %#v\nmt is: %#v\n\n", ms, mt)
 	return true
+}
+
+// my own solution about a year ago (November 2023)
+func isAnagram1(s string, t string) bool {
+	if len(s) != len(t) {
+		fmt.Printf("false: Not an Anagram. The length of strings [%#s] and [%#s] is not the same.\n\n", s, t)
+		return false
+	}
+
+	countS := map[rune]int{}
+	countT := map[rune]int{}
+
+	for _, c := range s {
+		_, ok := countS[c]
+		if ok == false {
+			countS[c] = 0
+		}
+		countS[c] += 1
+
+	}
+
+	for _, c := range t {
+		_, ok := countT[c]
+		if ok == false {
+			countT[c] = 0
+		}
+		countT[c] += 1
+
+	}
+
+	for key, _ := range countS {
+		_, ok := countT[key]
+
+		if ok == false || countT[key] != countS[key] {
+			fmt.Println("false: Not an Anagram.\n")
+			return false
+		}
+	}
+	fmt.Printf("true: \ncountS is: %#v\ncountT is: %#v\n\n", countS, countT)
+	return true
+
 }
